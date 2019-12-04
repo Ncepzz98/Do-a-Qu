26cdebbc09795bb186441ac84ea5ea298f209098
Ncepzz98 <08Agustus88>
2019-12-04 22:44:14 +0700

    Pertemuan 9

---
+++ app/src/main/java/id/kuliah/doaqu/Doanya.kt
@@ -0,0 +1,12 @@
+package id.kuliah.doaqu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Doanya : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doanya)
+    }
+}
